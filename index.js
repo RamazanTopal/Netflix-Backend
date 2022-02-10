c4ebf1f1caa1574dddafd229c847a320249ffdfe
Ramazan Topal <ramazantopal@iyimakina.com>
2022-02-10 21:03:24 +0300

    Add List api

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ dotenv.config();
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/users");
 const movieRoutes = require("./routes/movies");
+const listRoutes = require("./routes/lists");
 app.use(morgan('combined'))
 app.use(express.json());
 
@@ -22,7 +23,7 @@ mongoose.connect(process.env.MONGO_URL,{
 app.use("/api/auth",authRoutes)
 app.use("/api/users",userRoutes)
 app.use("/api/movies",movieRoutes)
-
+app.use("/api/lists",listRoutes)
 app.listen(8800,()=>{
   console.log("Backend server is running!")
 })
